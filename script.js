568caba21cfa9af7f428cfbaad512b4427103787
Masaru Iritani <25241373+masaru-iritani@users.noreply.github.com>
2023-11-15 00:28:08 +0900

    Allow changing the header of the second column when double-clicking it.

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
+    // Event listener for table body cells
     document.querySelectorAll('table td:nth-child(2)').forEach(cell => {
         cell.addEventListener('dblclick', function(e) {
             if (e.target !== this) return;
@@ -23,6 +24,27 @@ document.addEventListener('DOMContentLoaded', () => {
                     this.blur();
                 }
             });
+        });
+    });
+
+    // Event listener for table header cell
+    document.querySelector('table th:nth-child(2)').addEventListener('dblclick', function() {
+        const input = document.createElement('input');
+        input.type = 'text';
+        input.value = this.innerText;
+        this.innerText = '';
+        this.appendChild(input);
+        input.focus();
+
+        input.addEventListener('blur', function() {
+            const newHeader = this.value.trim() || 'Column 2';
+            this.parentElement.innerText = newHeader;
+        });
+
+        input.addEventListener('keydown', function(e) {
+            if (e.key === 'Enter') {
+                this.blur();
+            }
         });
     });
 });
