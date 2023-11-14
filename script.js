1d29f5c43669f077fe9b0e184382c030ce5bd4d1
Masaru Iritani <25241373+masaru-iritani@users.noreply.github.com>
2023-11-15 00:29:55 +0900

    Refer CSS and JavaScript from HTML.

--- script.js
+++ script.js
@@ -1,50 +1,40 @@
 document.addEventListener('DOMContentLoaded', () => {
-    // Event listener for table body cells
     document.querySelectorAll('table td:nth-child(2)').forEach(cell => {
-        cell.addEventListener('dblclick', function(e) {
-            if (e.target !== this) return;
-            const input = document.createElement('input');
-            input.type = 'number';
-            input.min = 0;
-            input.max = 59;
-            input.style.width = '100%';
-            input.value = '00';
-            this.appendChild(input);
-            input.focus();
-
-            input.addEventListener('blur', function() {
-                const span = document.createElement('span');
-                span.textContent = this.value.padStart(2, '0') + ', ';
-                cell.insertBefore(span, this);
-                cell.removeChild(this);
-            });
-
-            input.addEventListener('keydown', function(e) {
-                if (e.key === 'Enter') {
-                    this.blur();
-                }
-            });
-        });
+        cell.addEventListener('dblclick', handleTableBodyCellDblClick);
     });
-
-    // Event listener for table header cell
-    document.querySelector('table th:nth-child(2)').addEventListener('dblclick', function() {
-        const input = document.createElement('input');
-        input.type = 'text';
-        input.value = this.innerText;
-        this.innerText = '';
-        this.appendChild(input);
-        input.focus();
-
-        input.addEventListener('blur', function() {
-            const newHeader = this.value.trim() || 'Column 2';
-            this.parentElement.innerText = newHeader;
-        });
-
-        input.addEventListener('keydown', function(e) {
-            if (e.key === 'Enter') {
-                this.blur();
-            }
-        });
-    });
+    document.querySelector('table th:nth-child(2)').addEventListener('dblclick', handleTableHeaderCellDblClick);
 });
+
+function createEditableInput(cell, defaultValue, callback) {
+    const input = document.createElement('input');
+    input.type = 'number';
+    input.min = 0;
+    input.max = 59;
+    input.style.width = '100%';
+    input.value = defaultValue;
+    cell.appendChild(input);
+    input.focus();
+    input.addEventListener('blur', callback);
+    input.addEventListener('keydown', function(e) {
+        if (e.key === 'Enter') {
+            this.blur();
+        }
+    });
+}
+
+function handleTableBodyCellDblClick(e) {
+    if (e.target !== this) return;
+    createEditableInput(this, '00', function() {
+        const span = document.createElement('span');
+        span.textContent = this.value.padStart(2, '0') + ', ';
+        this.parentElement.insertBefore(span, this);
+        this.parentElement.removeChild(this);
+    });
+}
+
+function handleTableHeaderCellDblClick() {
+    createEditableInput(this, this.innerText, function() {
+        const newHeader = this.value.trim() || 'Column 2';
+        this.parentElement.innerText = newHeader;
+    });
+}
