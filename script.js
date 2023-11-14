9b18111a966b4a8e8084ccd30d0d3ad9ae5c9d66
Masaru Iritani <25241373+masaru-iritani@users.noreply.github.com>
2023-11-15 00:27:51 +0900

    Allow putting multiple numbers when double-clicking the empty area of a cell in the second column.

--- script.js
+++ script.js
@@ -1,18 +1,21 @@
 document.addEventListener('DOMContentLoaded', () => {
     document.querySelectorAll('table td:nth-child(2)').forEach(cell => {
-        cell.addEventListener('dblclick', function() {
+        cell.addEventListener('dblclick', function(e) {
+            if (e.target !== this) return;
             const input = document.createElement('input');
             input.type = 'number';
             input.min = 0;
             input.max = 59;
             input.style.width = '100%';
-            input.value = this.innerText || '00';
-            this.innerText = '';
+            input.value = '00';
             this.appendChild(input);
             input.focus();
 
             input.addEventListener('blur', function() {
-                cell.innerText = this.value.padStart(2, '0');
+                const span = document.createElement('span');
+                span.textContent = this.value.padStart(2, '0') + ', ';
+                cell.insertBefore(span, this);
+                cell.removeChild(this);
             });
 
             input.addEventListener('keydown', function(e) {
